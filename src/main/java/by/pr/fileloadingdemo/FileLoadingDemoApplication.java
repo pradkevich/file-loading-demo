0e39179a370626217729b0e7cb3dd6f67e28b049
Pavel Radkevich <pradkevich@elinext.com>
2019-05-02 16:57:10 +0300

    initial commit

---
+++ src/main/java/by/pr/fileloadingdemo/FileLoadingDemoApplication.java
@@ -0,0 +1,13 @@
+package by.pr.fileloadingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileLoadingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileLoadingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/pr/fileloadingdemo/FileLoadingDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package by.pr.fileloadingdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileLoadingDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
